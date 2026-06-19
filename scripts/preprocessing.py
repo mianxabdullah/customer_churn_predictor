75d272493cc4a3c144ab8312a44480f5d08cae2d
mianxabdullah <mian.abdullah.ayub@gmail.com>
2026-06-19 22:08:45 +0500

    label encoded binary categorical columns

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -10,3 +10,14 @@ df=df.drop(columns=['customerID'])
 #for eda
 df_raw=df.copy()
 
+from sklearn.preprocessing import LabelEncoder
+le=LabelEncoder()
+
+#label encoding for binary categorical columns
+df['Churn']=le.fit_transform(df['Churn'])
+df['gender']=le.fit_transform(df['gender'])
+# df['SeniorCitizen']=le.fit_transform(df['SeniorCitizen'])
+df['Partner']=le.fit_transform(df['Partner'])
+df['Dependents']=le.fit_transform(df['Dependents'])
+df['PhoneService']=le.fit_transform(df['PhoneService'])
+df['PaperlessBilling']=le.fit_transform(df['PaperlessBilling'])
